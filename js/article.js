ea458501c65de2788b9b8e1769b326c1cbeb7e1e
freestyle21 <qubaoming@gmail.com>
2013-04-01 20:41:13 +0800

    Update article.js

--- js/article.js
+++ js/article.js
@@ -1,9 +1,9 @@
 $(function() {
-	$(window).on("scroll", function(){
+	/*$(window).on("scroll", function(){
 		var windowScroll = $(window).scrollTop();
 		console.log(windowScroll);
 		$('.MsgBody').stop(true,true).animate({
 			'top':windowScroll + 'px'
 		},500);
-	});
+	});*/
 });
