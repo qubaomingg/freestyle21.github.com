b137aa041b772675cee9d91a486ca8dc2e797452
freestyle21 <675861708@qq.com>
2013-04-05 10:02:23 +0800

    fixed ie

--- js/article.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	/*$(window).on("scroll", function(){
-		var windowScroll = $(window).scrollTop();
-		console.log(windowScroll);
-		$('.MsgBody').stop(true,true).animate({
-			'top':windowScroll + 'px'
-		},500);
-	});*/
-});
